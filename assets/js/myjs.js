9fe3e6332359380ef499d33427328ec8c52426ab
prasojopxl <prasojodesigner@gmail.com>
2020-04-08 13:56:15 +0700

    v1

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,20 +1,18 @@
 $(document).ready(function () {
     new WOW().init();
-	$(function() {
-		$('.your-class').on('init', function(event, slick) {
-			$(this).append('<div class="slick-counter"><span class="current"></span> / <span class="total"></span></div>');
-			$('.current').text(slick.currentSlide + 1);
-			$('.total').text(slick.slideCount);
-		})
-		.slick({
-			dots: true,
-			arrows:true,
-			prevArrow: '<button class="slick-prev" aria-label="Previous" type="button">Sebelumnnya</button>',
-			nextArrow: '<button class="slick-next" aria-label="Next" type="button">Sesudahnya</button>',			
-		})
-		.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-			$('.current').text(nextSlide + 1);
-		});
-	});		    
+	$('#fullpage').fullpage({
+		autoScrolling: false,
+		fitToSection: false,
+		verticalCentered: true,
+	});
+	$(".product-slider").slick({
+		dots: false,
+		arrows:true,
+		prevArrow: '<div class="slick-prev"><img src="assets/images/arrow-right.svg" alt=""/></div>',
+		nextArrow: '<div class="slick-next"><img src="assets/images/arrow-left.svg" alt=""/></div>',
+		slidesToShow: 3,
+		slidesToScroll: 1,
+					
+	});	
 });
 
